a176a6d168a8d833b7889406a839b9ff0e3e561d
sebasthianB12 <sebasthian150519@outlook.com>
2023-11-21 20:44:52 -0500

    Primer Commit

---
+++ PresentacionFinal.py
@@ -0,0 +1,169 @@
+INICIAL = {
+    'Dotacion-2023': [
+        'Juega, crea, resuelve y aprende. Material impreso para niños y niñas de 4 años',
+        'Juega, crea, resuelve y aprende. Material impreso para niños y niñas de 5 años'
+    ],
+    'Aprendo-En-Casa': [
+        'Yo también me cuido: Aprendo en casa 2021 TV Inicial - 26 de abril',
+        'Yo cuido la naturaleza: Aprendo en casa 2021 TV Inicial - 15 de noviembre',
+        'Y tú, ¿Cómo cuidas la naturaleza? Aprendo en casa 2021 TV Inicial - 17 de noviembre',
+        'Vendemos, compramos o cambiamos: Aprendo en casa 2021 TV Inicial - 22 de octubre',
+        'Un viaje por el Perú desde: Aprendo en casa 2021 TV Inicial - 05 de julio',
+        'Un tiempo para divertirnos en familia: Aprendo en casa 2021 TV Inicial - 06 de setiembre',
+        'Un poema que rima: Aprendo en casa 2021 TV Inicial - 27 de abril',
+        'Un poder especial: Aprendo en casa 2021 TV Inicial - 06 de diciembre',
+        'Un pequeño intruso en casa: Aprendo en casa 2021 TV Inicial - 03 de mayo',
+        'Un pequeño accidente: Aprendo en casa 2021 TV Inicial - 12 de mayo',
+        'Tumbamos botellas: Aprendo en casa 2021 TV Inicial - 01 de setiembre',
+        'Todo lo que puedo hacer: Aprendo en casa 2021 TV Inicial - 23 de abril'
+    ],
+    'Sesiones-De-Aprendizaje-Videos': [
+        'Lectura aguita de vida',
+        'Y tú, ¿Cómo te lavas las manos?',
+        'Conversamos sobre las ferias, tiendas y mercados',
+        'Tiempo libre para jugar en familia',
+        'Cuido mi salud, practicando y promoviendo hábitos saludables',
+        'Estilo de vida saludable',
+        'Comida saludable mientras aprendo a contar',
+        'Características sensitivas de los alimentos',
+        '1,2,3 ¿Qué comeré esta vez?',
+        'Clasificación de frutas y verduras',
+        'Todo se puede transformar',
+        '¿Qué es lo que hay adentro?'
+    ]
+}
+
+PRIMARIA = {
+    'Ambito-rural-dotacion-2023': [
+        'Comunicación 1. mi cuaderno de autoaprendizaje',
+        'Comunicación 2. mi cuaderno de autoaprendizaje',
+        'Comunicación 3. mi cuaderno de autoaprendizaje',
+        'Comunicación 4. mi cuaderno de autoaprendizaje',
+        'Comunicación 5. mi cuaderno de autoaprendizaje',
+        'Comunicación 6. mi cuaderno de autoaprendizaje',
+        'Matemática 1. mi cuaderno de autoaprendizaje',
+        'Matemática 2. mi cuaderno de autoaprendizaje',
+        'Matemática 3. mi cuaderno de autoaprendizaje',
+        'Matemática 4. mi cuaderno de autoaprendizaje',
+        'Matemática 5. mi cuaderno de autoaprendizaje',
+        'Matemática 6. mi cuaderno de autoaprendizaje'
+    ],
+    'Aprendo-en-casa-primaria': [
+        'Presentación de la experiencia de aprendizaje Un presupuesto para estar saludables - 5. y 6. grado',
+        'Presentación de la experiencia de aprendizaje Diseñamos cometas - 3.er y 4. grado',
+        'Presentación de la situación Aprovechamos las ofertas con el uso de equivalencias - 5. y 6. grado',
+        'Recurso 1: Video de presentación de la situación Preparamos nuestras comidas típicas con el uso de proporciones',
+        'Recurso 1: Video de presentación de la situación Calculamos para compartir alimentos',
+        'Recurso Actividad 3: Let s read! - 5. y 6. grado',
+        'Recurso Actividad 2: Let s read! - 5. y 6. grado',
+        'Recurso Actividad 1: Let s read! - 5. y 6. grado',
+        'Recurso Actividad 1: Let s read!',
+        'Recurso Actividad 2: Let s read!'
+    ]
+}
+
+SECUNDARIA = {
+    'Regular-dotacion-2023': [
+        'Orientaciones pedagógicas para el uso de las fichas de matemática: Docente Secundaria',
+        'Orientaciones pedagógicas para el uso de aprendizaje de comunicación de 1, 2, 3, 4 y 5 de Educación Secundaria: Docente Secundaria',
+        'Fichas de matemática 1, secundaria',
+        'Fichas de matemática 2, secundaria',
+        'Fichas de matemática 3, secundaria',
+        'Fichas de matemática 4, secundaria',
+        'Fichas de matemática 5, secundaria',
+        'Fichas de aprendizaje de comunicación 1, secundaria',
+        'Fichas de aprendizaje de comunicación 2, secundaria',
+        'Fichas de aprendizaje de comunicación 3, secundaria',
+        'Fichas de aprendizaje de comunicación 4, secundaria',
+        'Fichas de aprendizaje de comunicación 5, secundaria'
+    ],
+    'Aprendo-en-casa-secundaria': [
+        'Elaboramos artefactos caseros ecológicos para prevenir enfermedades provocadas por una atmósfera contaminada: experiencia de aprendizaje 3, Educación para el trabajo. 5.grado',
+        'Prototipamos y evaluamos nuestra idea creativa: experiencia de aprendizaje 2, Educación para el trabajo, actividad 3 - 5. grado',
+        'Organizamos y sintetizamos la información para definir la necesidad del usuario: experiencia de aprendizaje 2, Educación para el trabajo, actividad 2 - 5. grado',
+        'Formulamos el desafío y empatizamos a partir de las potencialidades de nuestra localidad:  experiencia de aprendizaje 2, Educación para el trabajo, actividad 1 -  5.grado',
+        'Proponemos prototipos que promueven el trabajo en las familias, aprovechando las potencialidades de nuestra localidad: planificamos nuestra experiencia de aprendizaje 2, Educación para el trabajo. 5.',
+        'Proponemos prototipos que promueven el trabajo en las familias, aprovechando las potencialidades de nuestra localidad: experiencia de aprendizaje 2, Educación para el trabajo. 5. grado',
+        'Prototipamos y evaluamos aplicando la metodología Design Thinking: experiencia de aprendizaje 1, Educación para el trabajo, actividad 3 - 5. grado',
+        'Definimos el problema y generamos ideas creativas aplicando el Design Thinking: experiencia de aprendizaje 1, Educación para el trabajo - actividad 2. 5. grado',
+        'Formulamos el desafío y recogemos información en el marco de la metodología Design Thinking: experiencia de aprendizaje 1, Educación para el trabajo, actividad 1 - 5. grado',
+        'Creamos medios de comunicación novedosos para promover la participación masiva de los ciudadanos en las elecciones: planificamos nuestra experiencia de aprendizaje 1, Educación para el trabajo. 5.',
+        'Creamos medios de comunicación novedosos para promover la participación masiva de los ciudadanos en las elecciones: experiencia de aprendizaje 1, Educación para el trabajo. 5.grado',
+        'Evaluamos el prototipo e incorporamos mejoras para obtener el prototipo-solución: experiencia de aprendizaje 9, Educación para el trabajo, actividad 5 - 3. y 4. grado'
+    ]
+}
+
+LENGUAJE = {
+    'Intercultural-Bilingue': [
+        'Teesh 4. = Comunicación 4. Secundaria: Cuaderno de trabajo - Awajún',
+        'Ninkakananti nokon joi 4. = Comunicación 4. Secundaria: Cuaderno de trabajo - Shipibo-konibo',
+        'Shawi - Fichas de autoaprendizaje Mamaru del III Ciclo (1.er y 2.) de Primaria',
+        'Quechua Wanka - Fichas de autoaprendizaje Chachas del IV Ciclo (3.er) de Primaria',
+        'Quechua Central (Variante Wanka) - Fichas de autoaprendizaje Chachas del III Ciclo (1.er y 2.) de Primaria',
+        'Kichwa - Fichas de autoaprendizaje Mamaru del IV Ciclo (3.er) de Primaria',
+        'Quechua Amazónico (Kichwa) - Fichas de autoaprendizaje Mamaru del III Ciclo (1.er y 2.) de Primaria',
+        'Matsigenka - Fichas de autoaprendizaje Mamaru del IV Ciclo (3.er) de Primaria',
+        'Matsigenka - Fichas de autoaprendizaje Mamaru del III Ciclo (1.er y 2.) de Primaria',
+        'Tambante 2. Anquiadnuna = Matemática 2. Cuaderno de trabajo - Matsés'
+    ],
+    'Aprendo-en-casa-Intercultural-Bilingue': [
+        'Presentación 1. - Quechua inkawasi kañaris',
+        'Presentación 2. - Quechua collao',
+        'Presentación 1. - Quechua collao',
+        'Presentación 2. - Quechua chanka',
+        'Presentación 2. - Quechua Central',
+        'Presentación 1. - Quechua Central',
+        'Presentación 2. - Awajún',
+        'Presentación 1. - Awajún',
+        'Presentacion. - Quechua chanka',
+        'Practicamos castellano. - Castellano',
+        'Pista:Piratsipe. - Asháninka',
+        'Pista: Cinco pecesitos. - Asháninka'
+    ]
+}
+
+
+
+#METODOS:
+def mostrar_cursos(categorias_elegidas, datos_cursos):
+    for categoria in categorias_elegidas:
+        categoria = categoria.strip()
+        if categoria in datos_cursos:
+            print(f"\n{categoria.capitalize()}:")
+            for nombre_lista, cursos in datos_cursos[categoria].items():
+                print(f"\n    - {nombre_lista}:\n")
+                for i, nombre_curso in enumerate(cursos, start=1):
+                    print(f"        {i}. {nombre_curso}")
+        else:
+            print(f"\nNo hay datos disponibles para la categoría {categoria.capitalize()}.\n")
+
+
+
+
+
+#MUESTRA EN CONSOLA
+
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+print("Si quiere colocar mas de una categoria, coloque ',' luego de la categoria deseada (Inicial, Primaria, Secundaria)")
+categorias_elegidas = input("Ingrese sus categorías separadas por comas: ").lower().split(',')
+print(f"\nHola {nombre}, según las categorías seleccionadas, puede acceder a estos cursos:\n")
+
+mostrar_cursos(categorias_elegidas)
+
+
+
+
+
+
+
+
+
+
+def pruebaInicial(respuesta):
+
+    if respuesta == True:
+        print()
+
+    print()
+
